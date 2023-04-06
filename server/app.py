8aecf13398f6b847a5befc50b4b6fd5021969af0
Taylor Ambrose <taykesby@gmail.com>
2023-04-06 13:00:57 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -14,9 +14,11 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def home():
     return '<h1>Bakery GET-POST-PATCH-DELETE API</h1>'
+
 
 @app.route('/bakeries')
 def bakeries():
@@ -30,17 +32,102 @@ def bakeries():
     )
     return response
 
-@app.route('/bakeries/<int:id>')
+
+@app.route('/bakeries/<int:id>', methods=['GET', 'PATCH'])
 def bakery_by_id(id):
+    if request.method == 'GET':
+        bakery = Bakery.query.filter_by(id=id).first()
+        bakery_serialized = bakery.to_dict()
 
-    bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
+        response = make_response(
+            bakery_serialized,
+            200
+        )
+        return response
 
-    response = make_response(
-        bakery_serialized,
-        200
-    )
-    return response
+    elif request.method == 'PATCH':
+        bakery = Bakery.query.filter(Bakery.id == id).first()
+
+        for attr in request.form:
+            setattr(bakery, attr, request.form.get(attr))
+
+        db.session.add(bakery)
+        db.session.commit()
+
+        bakery_dict = bakery.to_dict()
+
+        response = make_response(
+            bakery_dict,
+            200
+        )
+        return response
+###################################################################
+
+
+@app.route('/baked_goods', methods=['GET', 'POST', 'DELETE'])
+def baked_goods():
+    if request.method == 'GET':
+        baked_goods = []
+        for baked_good in BakedGood.query.all():
+            baked_good_dict = baked_good.to_dict()
+            baked_goods.append(baked_good_dict)
+        response = make_response(
+            baked_goods,
+            200
+        )
+        return response
+
+    elif request.method == 'POST':
+        new_baked_good = BakedGood(
+            name=request.form.get("name"),
+            price=request.form.get("price"),
+            created_at=request.form.get("created_at"),
+            updated_at=request.form.get("updated_at"),
+            bakery_id=request.form.get("bakery_id")
+        )
+
+        db.session.add(new_baked_good)
+        db.session.commit()
+
+        baked_good_dict = new_baked_good.to_dict()
+
+        response = make_response(
+            baked_good_dict,
+            201
+        )
+
+        return response
+
+
+@app.route('/baked_goods/<int:id>', methods=['GET', 'DELETE'])
+def baked_goods_by_id(id):
+    baked_good = BakedGood.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        baked_good_serialized = baked_good.to_dict()
+
+        response = make_response(
+            baked_good_serialized,
+            200
+        )
+        return response
+
+    elif request.method == 'DELETE':
+        db.session.delete(baked_good)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Baked Good Deleted."
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
+###################################################################
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -48,16 +135,18 @@ def baked_goods_by_price():
     baked_goods_by_price_serialized = [
         bg.to_dict() for bg in baked_goods_by_price
     ]
-    
+
     response = make_response(
         baked_goods_by_price_serialized,
         200
     )
     return response
 
+
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    most_expensive = BakedGood.query.order_by(
+        BakedGood.price.desc()).limit(1).first()
     most_expensive_serialized = most_expensive.to_dict()
 
     response = make_response(
@@ -65,6 +154,9 @@ def most_expensive_baked_good():
         200
     )
     return response
+
+##########################################################################################
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
---
+++ server/migrations/versions/7418bcc6e9cf_initial_create_tables.py
@@ -0,0 +1,24 @@
+"""initial create tables
+
+Revision ID: 7418bcc6e9cf
+Revises: b6aec9715b77
+Create Date: 2023-04-06 11:42:37.728281
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7418bcc6e9cf'
+down_revision = 'b6aec9715b77'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
